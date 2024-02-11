38a51f17154451ffbe5f12e1ed24e3487cbb3d1e
Justin Oh <justinoh4116@gmail.com>
2024-02-11 06:12:43 +0000

    initial dunst config

---
+++ users/justin/programs/fish.nix
@@ -0,0 +1,11 @@
+args@{...}:
+
+programs.fish = {
+  enable = true;
+  plugins = [
+    { name = "tide"; src = pkgs.fishPlugins.tide.src; }
+    { name = "z"; src = pkgs.fishPlugins.z.src; }
+    { name = "puffer"; src = pkgs.fishPlugins.puffer.src; }
+    { name = "fzf-fish"; src = pkgs.fishPlugins.fzf-fish.src; }
+  ];
+};
