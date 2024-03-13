ac67f8620d006948579692da234c35cc41d22ca2
Justin Oh <justinoh4116@gmail.com>
2024-03-12 23:44:10 -0700

    39 current  2024-03-12 23:44:06  24.05.20240303.b8697e5  6.7.7                           *

--- hosts/framework/keyboard.nix
+++ hosts/framework/keyboard.nix
@@ -12,5 +12,15 @@
 
   services.kmonad = {
     enable = true;
+
+    keyboards.internal = {
+      device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
+      config = builtins.readFile ./dots/framework.kbd;
+
+      defcfg = {
+        enable = true;
+        fallthrough = true;
+      };
+    };
   };
 }
