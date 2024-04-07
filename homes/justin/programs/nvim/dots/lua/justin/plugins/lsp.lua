2a4b4d4305798c7a8ae1bc85b917d9200d828448
Justin Oh <justinoh4116@gmail.com>
2024-04-06 17:43:14 -0700

    2024-04-06 17:43 : id 165 -> /nix/store/3wy6zkldmhjfjjcwpql65pcad3ygk9bq-home-manager-generation

--- homes/justin/programs/nvim/dots/lua/justin/plugins/lsp.lua
+++ homes/justin/programs/nvim/dots/lua/justin/plugins/lsp.lua
@@ -13,65 +13,91 @@ return
                 'hrsh7th/cmp-nvim-lsp',
                 config = true,
                 dependencies = {
-                    'williamboman/mason-lspconfig.nvim',
-                    config = true,
-                    dependencies = {
-                        'williamboman/mason.nvim',
-                        config = true,
+                    {
+                        'VonHeikemen/lsp-zero.nvim',
+                        branch = 'v3.x',
+                        lazy = true,
+                        config = false,
+                        init = function()
+                            -- Disable automatic setup, we are doing it manually
+                            vim.g.lsp_zero_extend_cmp = 0
+                            vim.g.lsp_zero_extend_lspconfig = 0
+                        end,
                     },
+                    -- 'williamboman/mason-lspconfig.nvim',
+                    -- config = true,
+                    -- dependencies = {
+                    --     'williamboman/mason.nvim',
+                    --     config = true,
+                    -- },
                 },
             },
         },
         config = function()
-            local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
-            local lsp_attach = function(_, bufnr)
-                vim.keymap.set('n', '<leader>cd', vim.diagnostic.open_float, { buffer = bufnr })
-                vim.keymap.set('n', '<leader>cl', '<cmd>LspInfo<cr>', { buffer = bufnr })
-                vim.keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<cr>', { buffer = bufnr })
-                vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = bufnr })
-                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = bufnr })
-                vim.keymap.set('n', 'gI', '<cmd>Telescope lsp_implementations<cr>', { buffer = bufnr })
-                vim.keymap.set('n', 'gt', '<cmd>Telescope lsp_type_definitions<cr>', { buffer = bufnr })
-                vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr })
-                vim.keymap.set('n', 'gK', vim.lsp.buf.signature_help, { buffer = bufnr })
-                vim.keymap.set('i', '<c-k>', vim.lsp.buf.signature_help, { buffer = bufnr })
-            end
+            -- This is where all the LSP shenanigans will live
+            local lsp_zero = require('lsp-zero')
+            lsp_zero.extend_lspconfig()
 
-            vim.diagnostic.config({
-                -- virtual_text = false,
-            })
+            lsp_zero.on_attach(function(client, bufnr)
+                -- see :help lsp-zero-keybindings
+                -- to learn the available actions
+                lsp_zero.default_keymaps({buffer = bufnr})
+            end)
 
-            local lspconfig = require('lspconfig')
-            require('mason-lspconfig').setup_handlers({
-                function(server_name)
-                    lspconfig[server_name].setup({
-                        on_attach = lsp_attach,
-                        capabilities = lsp_capabilities,
-                    })
-                end,
-                ['rust_analyzer'] = function ()
-                    require('rust-tools').setup({
-                        server = {
-                            capabilities = lsp_capabilities,
-                            on_attach = function(_, bufnr)
-                                vim.keymap.set('n', '<leader>cd', vim.diagnostic.open_float, { buffer = bufnr })
-                                vim.keymap.set('n', '<leader>cl', '<cmd>LspInfo<cr>', { buffer = bufnr })
-                                vim.keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<cr>', { buffer = bufnr })
-                                vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = bufnr })
-                                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = bufnr })
-                                vim.keymap.set('n', 'gI', '<cmd>Telescope lsp_implementations<cr>', { buffer = bufnr })
-                                vim.keymap.set('n', 'gt', '<cmd>Telescope lsp_type_definitions<cr>', { buffer = bufnr })
-                                vim.keymap.set('n', 'K', '<cmd>RustHoverActions<cr>', { buffer = bufnr })
-                                vim.keymap.set('n', 'gK', vim.lsp.buf.signature_help, { buffer = bufnr })
-                                vim.keymap.set('i', '<c-k>', vim.lsp.buf.signature_help, { buffer = bufnr })
-                            end,
-                        },
-                    lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
-                    })
-                    -- require'rust-tools'.hover_actions.hover_actions()
-                    -- require('rust-tools').runnables.runnables()
-                end
-            })
-        end,
+            -- (Optional) Configure lua language server for neovim
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            require('lspconfig').lua_ls.setup(lua_opts)
+        end
+        -- config = function()
+        --     local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+        --     local lsp_attach = function(_, bufnr)
+        --         vim.keymap.set('n', '<leader>cd', vim.diagnostic.open_float, { buffer = bufnr })
+        --         vim.keymap.set('n', '<leader>cl', '<cmd>LspInfo<cr>', { buffer = bufnr })
+        --         vim.keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<cr>', { buffer = bufnr })
+        --         vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = bufnr })
+        --         vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = bufnr })
+        --         vim.keymap.set('n', 'gI', '<cmd>Telescope lsp_implementations<cr>', { buffer = bufnr })
+        --         vim.keymap.set('n', 'gt', '<cmd>Telescope lsp_type_definitions<cr>', { buffer = bufnr })
+        --         vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr })
+        --         vim.keymap.set('n', 'gK', vim.lsp.buf.signature_help, { buffer = bufnr })
+        --         vim.keymap.set('i', '<c-k>', vim.lsp.buf.signature_help, { buffer = bufnr })
+        --     end
+
+        --     vim.diagnostic.config({
+        --         -- virtual_text = false,
+        --     })
+
+        --     local lspconfig = require('lspconfig')
+        --     require('mason-lspconfig').setup_handlers({
+        --         function(server_name)
+        --             lspconfig[server_name].setup({
+        --                 on_attach = lsp_attach,
+        --                 capabilities = lsp_capabilities,
+        --             })
+        --         end,
+        --         ['rust_analyzer'] = function ()
+        --             require('rust-tools').setup({
+        --                 server = {
+        --                     capabilities = lsp_capabilities,
+        --                     on_attach = function(_, bufnr)
+        --                         vim.keymap.set('n', '<leader>cd', vim.diagnostic.open_float, { buffer = bufnr })
+        --                         vim.keymap.set('n', '<leader>cl', '<cmd>LspInfo<cr>', { buffer = bufnr })
+        --                         vim.keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<cr>', { buffer = bufnr })
+        --                         vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = bufnr })
+        --                         vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = bufnr })
+        --                         vim.keymap.set('n', 'gI', '<cmd>Telescope lsp_implementations<cr>', { buffer = bufnr })
+        --                         vim.keymap.set('n', 'gt', '<cmd>Telescope lsp_type_definitions<cr>', { buffer = bufnr })
+        --                         vim.keymap.set('n', 'K', '<cmd>RustHoverActions<cr>', { buffer = bufnr })
+        --                         vim.keymap.set('n', 'gK', vim.lsp.buf.signature_help, { buffer = bufnr })
+        --                         vim.keymap.set('i', '<c-k>', vim.lsp.buf.signature_help, { buffer = bufnr })
+        --                     end,
+        --                 },
+        --             lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+        --             })
+        --             -- require'rust-tools'.hover_actions.hover_actions()
+        --             -- require('rust-tools').runnables.runnables()
+        --         end
+        --     })
+        -- end,
     },
 }
