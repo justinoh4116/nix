8c12352f58c9b0d04d618b7fec0c85fc61663144
Justin Oh <justinoh4116@gmail.com>
2024-04-06 18:08:44 -0700

    2024-04-06 18:08 : id 170 -> /nix/store/dhwgzs5s3gl5mq154kldlcnzi5pkq0v1-home-manager-generation

--- homes/justin/programs/nvim/dots/lua/justin/plugins/fidget.lua
+++ homes/justin/programs/nvim/dots/lua/justin/plugins/fidget.lua
@@ -3,9 +3,11 @@ return {
     tag = 'v1.0.0',
     event = { 'BufReadPre', 'BufNewFile' },
     opts = {
-        display = {
-            progress_icon = {
-                pattern = "pipe",
+        progress = {
+            display = {
+                progress_icon = {
+                    pattern = "pipe",
+                },
             },
         },
     },
