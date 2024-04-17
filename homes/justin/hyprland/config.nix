586d65580e3680c4378f97ba8a8f957d052383c6
Justin Oh <justinoh4116@gmail.com>
2024-04-16 22:04:17 -0700

    2024-04-16 22:04 : id 183 -> /nix/store/9rx68qk1ziggh9b6rjp35vr2qxrd1yb6-home-manager-generation

--- homes/justin/hyprland/config.nix
+++ homes/justin/hyprland/config.nix
@@ -199,6 +199,7 @@
         "float, class:^(anyrun)$"
         "float, class:^(firefox)$, title:^(Sign in - Google Accounts).*"
         "float, class:^(KiCad)$, title: .*(Symbol Editor)$"
+        "float, class:^(Zotero)$, title .*(Citation)$"
       ];
 
       layerrule = [
