3b10fa81c784cc12b5103248fd086089988f7b48
Justin Oh <justinoh4116@gmail.com>
2024-05-13 20:36:37 -0700

    2024-05-13 20:36 : id 192 -> /nix/store/3vf4w70lhk6k0f35n2zr2hqab87sr47w-home-manager-generation

--- homes/justin/misc/default.nix
+++ homes/justin/misc/default.nix
@@ -1,5 +1,25 @@
 {
+  self,
+  inputs,
+  config,
+  opts,
+  ...
+}: {
   imports = [
     ./dconf.nix
   ];
+
+  config = {
+    home.xdg.mimeApps = {
+      enable = true;
+
+      defaultApplications = {
+        "text/html" = "org.chromium.chromium.desktop";
+        "x-scheme-handler/http" = "org.chromium.chromium.desktop";
+        "x-scheme-handler/https" = "org.chromium.chromium.desktop";
+        "x-scheme-handler/about" = "org.chromium.chromium.desktop";
+        "x-scheme-handler/unknown" = "org.chromium.chromium.desktop";
+      };
+    };
+  };
 }
