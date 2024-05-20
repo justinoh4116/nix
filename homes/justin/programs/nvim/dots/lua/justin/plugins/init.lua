e0a50007398ee1d1bbef481501afa1c7d517a3dd
Justin Oh <justinoh4116@gmail.com>
2024-05-20 14:43:21 -0700

    2024-05-20 14:43 : id 202 -> /nix/store/vy91x3z338qh2lrs5v9cxnrz9g8kr2kc-home-manager-generation

--- homes/justin/programs/nvim/dots/lua/justin/plugins/init.lua
+++ homes/justin/programs/nvim/dots/lua/justin/plugins/init.lua
@@ -54,6 +54,7 @@ return {
             vim.g.vimtex_view_method='zathura'
             vim.g.vimtex_quickfix_mode=0
             vim.opt.conceallevel=0
+            vim.g.vimtex_fold_enabled=0
             -- vim.g.tex_conceal='abdmg'
         end
 
