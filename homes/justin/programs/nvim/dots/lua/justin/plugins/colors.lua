53d9fb581eac1b61f6342da23130bb5c8103e981
Justin Oh <justinoh4116@gmail.com>
2024-07-01 23:12:56 -0400

    2024-07-01 23:12 : id 227 -> /nix/store/v42238hs35yy9mskjf3ajjc0g6z8sd47-home-manager-generation

--- homes/justin/programs/nvim/dots/lua/justin/plugins/colors.lua
+++ homes/justin/programs/nvim/dots/lua/justin/plugins/colors.lua
@@ -1,9 +1,17 @@
+-- Sets colors to line numbers Above, Current and Below  in this order
+function LineNumberColors()
+  vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#51B3EC', bold=true })
+  vim.api.nvim_set_hl(0, 'LineNr', { fg='white', bold=true })
+  vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#FB508F', bold=true })
+end
+
 return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     opts = {},
     config = function()
-        vim.cmd[[colorscheme tokyonight]]
+        vim.cmd[[colorscheme tokyonight-night]]
+        LineNumberColors()
     end,
 }
