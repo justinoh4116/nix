d60437f5958771ea5c16c0b7664b619e48431765
Justin Oh <justinoh4116@gmail.com>
2024-07-07 12:41:58 -0400

    2024-07-07 12:41 : id 260 -> /nix/store/jlz7wdgn3nif1mdl05nwyndsynwq05mf-home-manager-generation

---
+++ homes/justin/programs/nvim/dots/lua/justin/plugins/conform.lua
@@ -0,0 +1,14 @@
+return {
+  'stevearc/conform.nvim',
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      -- Conform will run multiple formatters sequentially
+      python = { "isort", "black" },
+      -- Use a sub-list to run only the first available formatter
+      javascript = { { "prettierd", "prettier" } },
+      nix = { 'alejandra' },
+    },
+  },
+  keys = { '<F3>', '<cmd>require("conform").format()<CR>' }
+}
