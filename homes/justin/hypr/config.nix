8b52eb3e8a646dada3c288ebe8b4bf1ba8f1c1b8
Justin Oh <justinoh4116@gmail.com>
2024-07-20 20:18:26 -0400

    2024-07-20 20:18 : id 296 -> /nix/store/43yrgqyb2hsg0y4g94b11v7331x016zy-home-manager-generation

--- homes/justin/hypr/config.nix
+++ homes/justin/hypr/config.nix
@@ -49,7 +49,13 @@
 
       gestures = {
         workspace_swipe = true;
+        workspace_swipe_distance = 700;
         workspace_swipe_fingers = 4;
+        workspace_swipe_cancel_ratio = 0.2;
+        workspace_swipe_min_speed_to_force = 5;
+        workspace_swipe_direction_lock = true;
+        workspace_swipe_direction_lock_threshold = 10;
+        workspace_swipe_create_new = true;
       };
 
       input = {
@@ -313,7 +319,7 @@
         hyprexpo = {
           columns = 3;
           gap_size = 5;
-          bg_col = "rgb(111111)";
+          bg_col = "rgb(0000000)";
           workspace_method = "first 1";
 
           enable_gesture = true;
