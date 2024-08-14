eb2111d9944498a4fc73ee228408a8ac08512d39
Justin Oh <justinoh4116@gmail.com>
2024-08-14 12:11:29 -0700

    2024-08-14 12:11 : id 387 -> /nix/store/0fl4xbmh5r3nfjy9sp9240304xg5s66w-home-manager-generation

--- homes/justin/programs/nvim/dots/lua/justin/remap.lua
+++ homes/justin/programs/nvim/dots/lua/justin/remap.lua
@@ -1,6 +1,10 @@
 -- center screen when using ctrl d and u vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 
+-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
 -- center screen when navigating search results
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
