5b3fb3d94354b85f85af04390c752e5e59eb83fe
Justin Oh <justinoh4116@gmail.com>
2024-08-18 11:40:06 -0700

    88 current  2024-08-18 11:40:03  24.11.20240814.c3aa7b8  6.10.4                          *

--- hosts/framework/system.nix
+++ hosts/framework/system.nix
@@ -7,7 +7,7 @@
 }: {
   programs.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    # package = inputs.hyprland.packages.${pkgs.system}.hyprland;
   };
   networking.firewall = {
     trustedInterfaces = ["tailscale0"];
