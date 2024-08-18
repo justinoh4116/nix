ed8e62bfaece4540596c3cb38782c20ca716b2ec
Justin Oh <justinoh4116@gmail.com>
2024-08-17 20:54:24 -0700

    2024-08-17 20:54 : id 393 -> /nix/store/ss0wshacznf69xqg53rjqpii6afdw4c6-home-manager-generation

--- homes/justin/hypr/config.nix
+++ homes/justin/hypr/config.nix
@@ -46,9 +46,9 @@ in {
       ];
 
       monitor = [
-        "eDP-1, preferred, 0x0, 1.175, bitdepth, 10"
-        "DP-10, preferred, 1928x0, 1"
-        "DP-9, preferred, 3848x0, 2"
+        # "eDP-1, preferred, 0x0, 1.175, bitdepth, 10"
+        # "DP-10, preferred, 1928x0, 1"
+        # "DP-9, preferred, 3848x0, 2"
         ",preferred,auto,1"
       ];
 
