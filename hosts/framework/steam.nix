8efbd1c857ce2c48e73407f9f81b581b3ef42ecd
Justin Oh <justinoh4116@gmail.com>
2024-08-24 16:59:46 -0700

    92 current  2024-08-24 16:59:42  24.11.20240814.c3aa7b8  6.10.4                          *

--- hosts/framework/steam.nix
+++ hosts/framework/steam.nix
@@ -6,7 +6,25 @@
   lib,
   ...
 }: {
+  nixpkgs.config.packageOverrides = pkgs: {
+    steam = pkgs.steam.override {
+      extraPkgs = pkgs:
+        with pkgs; [
+          zlib
+          dbus
+          freetype
+          glib
+          atk
+          cairo
+          pango
+          fontconfig
+          libpng
+          icu
+        ];
+    };
+  };
   nixpkgs.config.allowUnfreePredicate = _: true;
+
   programs.steam = {
     enable = true;
   };
