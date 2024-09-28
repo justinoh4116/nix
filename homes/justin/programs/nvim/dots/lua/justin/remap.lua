27e9eb0be321386f6108c899f3507055bc4e28b0
Justin Oh <justinoh4116@gmail.com>
2024-09-28 00:14:00 -0700

    2024-09-28 00:13 : id 425 -> /nix/store/762n4milkhz1hvbmi2rk91msf1wkdwpj-home-manager-generation

--- homes/justin/programs/nvim/dots/lua/justin/remap.lua
+++ homes/justin/programs/nvim/dots/lua/justin/remap.lua
@@ -2,8 +2,8 @@
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 
 -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+-- vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+-- vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 
 -- center screen when navigating search results
 vim.keymap.set("n", "n", "nzzzv")
