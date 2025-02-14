62238659a52644aba2a2bc683d42d5be3b2227cf
Justin Oh <justinoh4116@gmail.com>
2025-02-13 20:35:45 -0800

    106 current  2025-02-13 17:06:46  25.05.20250204.799ba5b  6.13.1                          *

--- hosts/iceberg/caddy.nix
+++ hosts/iceberg/caddy.nix
@@ -7,7 +7,7 @@
   services.caddy = {
     enable = true;
     virtualHosts."photos.spicanet.duckdns.org".extraConfig = ''
-      reverse_proxy 192.168.0.6:2283
+      reverse_proxy 192.168.0.5:2283
     '';
   };
 
