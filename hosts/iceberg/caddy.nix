12f64382228a8ffc61e1e97792f71c1bee2d1f0e
Justin Oh <justinoh4116@gmail.com>
2025-02-13 20:18:26 -0800

    106 current  2025-02-13 17:06:46  25.05.20250204.799ba5b  6.13.1                          *

--- hosts/iceberg/caddy.nix
+++ hosts/iceberg/caddy.nix
@@ -4,30 +4,10 @@
   pkgs,
   ...
 }: {
-  containers.caddy = {
-    autoStart = true;
-    privateNetwork = true;
-    hostAddress = "192.168.100.3";
-    localAddress = "192.168.100.4";
-    bindMounts = {
-    };
-    config = let
-      hostConfig = config;
-    in
-      {
-        config,
-        pkgs,
-        ...
-      }: {
-        networking.useHostResolvConf = false;
-        system.stateVersion = "24.11";
-
-        services.caddy = {
-          enable = true;
-          virtualHosts."photos.spicanet.duckdns.org".extraConfig = ''
-            reverse_proxy https://192.168.0.6:2283
-          '';
-        };
-      };
+  services.caddy = {
+    enable = true;
+    virtualHosts."photos.spicanet.duckdns.org".extraConfig = ''
+      reverse_proxy https://192.168.0.6:2283
+    '';
   };
 }
