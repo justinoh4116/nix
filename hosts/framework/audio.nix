c01ba293f02459781d03d15d591bc424f5763e71
Justin Oh <justinoh4116@gmail.com>
2025-07-10 00:08:07 +0900

    49          2025-07-10 00:07:57  25.11.20250630.3016b4b     6.15.4   Unknown                 []              True

--- hosts/framework/audio.nix
+++ hosts/framework/audio.nix
@@ -19,26 +19,26 @@
     # alsa.enable = true;
     # jack.enable = true;
     # alsa.support32Bit = true;
-    wireplumber = {
-      enable = true;
-      #   extraConfig = {
-      #   "monitor.bluez.properties" = {
-      #     "bluez5.enable-sbc-xq" = true;
-      #     "bluez5.enable-msbc" = true;
-      #     "bluez5.enable-hw-volume" = true;
-      #     "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
-      #   };
-      # };
-      configPackages = [
-        (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/10-bluez.conf" ''
-          monitor.bluez.properties = {
-            bluez5.roles = [ a2dp_sink a2dp_source bap_sink bap_source hsp_hs hsp_ag hfp_hf hfp_ag ]
-            bluez5.codecs = [ sbc sbc_xq aac ]
-            bluez5.enable-sbc-xq = true
-            bluez5.hfphsp-backend = "native"
-          }
-        '')
-      ];
-    };
+    # wireplumber = {
+    #   enable = true;
+    #   #   extraConfig = {
+    #   #   "monitor.bluez.properties" = {
+    #   #     "bluez5.enable-sbc-xq" = true;
+    #   #     "bluez5.enable-msbc" = true;
+    #   #     "bluez5.enable-hw-volume" = true;
+    #   #     "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
+    #   #   };
+    #   # };
+    #   configPackages = [
+    #     (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/10-bluez.conf" ''
+    #       monitor.bluez.properties = {
+    #         bluez5.roles = [ a2dp_sink a2dp_source bap_sink bap_source hsp_hs hsp_ag hfp_hf hfp_ag ]
+    #         bluez5.codecs = [ sbc sbc_xq aac ]
+    #         bluez5.enable-sbc-xq = true
+    #         bluez5.hfphsp-backend = "native"
+    #       }
+    #     '')
+    #   ];
+    # };
   };
 }
