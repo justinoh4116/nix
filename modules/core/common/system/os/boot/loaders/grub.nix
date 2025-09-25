9ebe2e862773175ec97f3253cc8aeb4310cdd669
Justin Oh <justinoh4116@gmail.com>
2025-09-25 08:58:24 -0700

    85          2025-09-23 14:07:03  25.11.20250814.fbcf476  6.15.9  c7cdd40-dirty           []              True

---
+++ modules/core/common/system/os/boot/loaders/grub.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib) mkDefault mkIf optionalAttrs;
+
+  sys = config.modules.system;
+in {
+  config = mkIf (sys.boot.loader == "grub") {
+    boot.loader.grub = {
+    enable = true;
+    zfsSupport = sys.fs.zfs.enable;
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+    #device = "nodev";
+    mirroredBoots = [
+      {
+        devices = ["nodev"];
+        path = "/boot";
+      }
+    ];
+  };
+  };
+}
