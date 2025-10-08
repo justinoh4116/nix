d8b63bb83af44db0da84010dd1b80330b93133d9
Justin Oh <justinoh4116@gmail.com>
2025-10-08 10:00:24 -0700

    110         2025-10-08 10:00:16  25.11.20250924.e643668  6.16.8  3d05228-dirty           []              True

--- homes/justin/programs/graphical/floorp.nix
+++ homes/justin/programs/graphical/floorp.nix
@@ -3,16 +3,28 @@
   osConfig,
   pkgs,
   ...
-}: {
-  # programs.floorp.enable = true;
-  home.packages = with pkgs; [
-    (floorp-bin.override {
-      extraPrefsFiles = [
-        (builtins.fetchurl {
-          url = "https://raw.githubusercontent.com/MrOtherGuy/fx-autoconfig/master/program/config.js";
-          sha256 = "1mx679fbc4d9x4bnqajqx5a95y1lfasvf90pbqkh9sm3ch945p40";
-        })
-      ];
-    })
-  ];
+}: let
+  floorp = pkgs.floorp-bin.override {
+    extraPrefsFiles = [
+      (builtins.fetchurl {
+        url = "https://raw.githubusercontent.com/MrOtherGuy/fx-autoconfig/master/program/config.js";
+        sha256 = "1mx679fbc4d9x4bnqajqx5a95y1lfasvf90pbqkh9sm3ch945p40";
+      })
+    ];
+  };
+in {
+  programs.floorp = {
+    enable = true;
+    package = floorp;
+  };
+  # home.packages = with pkgs; [
+  #   (floorp-bin-unwrapped.override {
+  #     extraPrefsFiles = [
+  #       (builtins.fetchurl {
+  #         url = "https://raw.githubusercontent.com/MrOtherGuy/fx-autoconfig/master/program/config.js";
+  #         sha256 = "1mx679fbc4d9x4bnqajqx5a95y1lfasvf90pbqkh9sm3ch945p40";
+  #       })
+  #     ];
+  #   })
+  # ];
 }
