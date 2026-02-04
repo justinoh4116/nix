7c0f77b9b0ad4ba485d027d53ca4762844b2f852
Justin Oh <justinoh4116@gmail.com>
2026-02-03 17:30:17 -0800

    32          2026-02-03 17:30:10  26.05.20260126.bfc1b8a   6.18.7  88bc469-dirty           []              True

--- homes/justin/programs/nvim/dots/lua/justin/plugins/jupynium.lua
+++ homes/justin/programs/nvim/dots/lua/justin/plugins/jupynium.lua
@@ -1,7 +1,15 @@
-return {
-	"kiyoon/jupynium.nvim",
-	-- build = "pip3 install --user .",
-	build = "uv pip install . ", ----python=$HOME/.virtualenvs/jupynium/bin/python",
-	-- build = "conda run --no-capture-output -n jupynium pip install .",
-	ft = { "ipynb", "ju.py" },
-}
+if vim.fn.executable("jupynium") == 1 then -- only active when jupynium is available
+	return {
+		"kiyoon/jupynium.nvim",
+		-- build = "pip3 install --user .",
+		build = "uv pip install . ", ----python=$HOME/.virtualenvs/jupynium/bin/python",
+		-- build = "conda run --no-capture-output -n jupynium pip install .",
+		-- ft = { "ipynb", "ju.py" },
+		config = function()
+			require("jupynium.nvim").setup({
+				-- Configuration here, or leave empty to use defaults
+				python_host = "python",
+			})
+		end,
+	}
+end
