6a3e64ab6df687ecbafa6e4838ec3aa8dcbf81ff
Justin Oh <justinoh4116@gmail.com>
2026-02-19 11:12:00 -0800

    75          2026-02-19 11:11:54  26.05.20260126.bfc1b8a   6.18.7  1ec8b78-dirty           []              True

--- homes/justin/programs/nvim/dots/lua/justin/plugins/conform.lua
+++ homes/justin/programs/nvim/dots/lua/justin/plugins/conform.lua
@@ -1,15 +1,23 @@
 return {
 	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
 			-- Conform will run multiple formatters sequentially
-			python = { "isort", { "ruff_format", "black" } },
+			python = { "isort", "ruff_format", "black" },
 			-- Use a sub-list to run only the first available formatter
-			javascript = { { "prettierd", "prettier" } },
+			javascript = { "prettierd", "prettier" },
 			nix = { "alejandra" },
 			sv = { "verible" },
 			v = { "verible" },
+			cpp = { "clang-format" },
+			h = { "clang-format" },
+			c = { "clang-format" },
+		},
+		default_format_opts = {
+			lsp_format = "fallback",
 		},
 	},
 	keys = {
