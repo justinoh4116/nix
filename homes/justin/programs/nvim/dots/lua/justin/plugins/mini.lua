f70bca6bb41f09cc750b6c730b886bd770c06e0c
Justin Oh <justinoh4116@gmail.com>
2026-04-07 10:32:47 -0700

    130         2026-04-07 10:32:43  26.05.20260405.68d8aa3  6.19.11  d4dc88c-dirty           []              True

---
+++ homes/justin/programs/nvim/dots/lua/justin/plugins/mini.lua
@@ -0,0 +1,11 @@
+return {
+	"nvim-mini/mini.nvim",
+	version = false,
+	config = function()
+		require("mini.indentscope").setup()
+		require("mini.pairs").setup()
+		require("mini.keymap").setup()
+		require("mini.ai").setup()
+		require("mini.surround").setup()
+	end,
+}
